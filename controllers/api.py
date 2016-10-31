73797b8ffcee71069d158fc003ef47a07b00614a
Kyle Cilia <kylecilia@gmail.com>
2016-10-31 00:01:16 -0700

    Initial
    Initial

--- controllers/api.py
+++ controllers/api.py
@@ -5,7 +5,30 @@ def get_posts():
     that the first time, we get 4 posts max, and each time the "load more" button is pressed,
     we load at most 4 more posts."""
     # Implement me!
-    return response.json(dict())
+    start_i = int(request.vars.start_i) if request.vars.start_i is not None else 0
+    end_i = int(request.vars.end_i) if request.vars.end_i is not None else 0
+    posts = []
+    has_more = False
+    rows = db().select(db.post.ALL, limitby=(start_i, end_i + 1))
+    for i, r in enumerate(rows):
+        if i < end_i - start_i:
+            t = dict(
+                id = r.id,
+                user_email = r.user_email,
+                post_content = r.post_content,
+                created_on = r.created_on,
+                updated_on = r.updated_on,
+            )
+            posts.append(t)
+        else:
+            has_more = True
+    logged_in = auth.user_id is not None
+
+    return response.json(dict(
+        posts = posts,
+        logged_in=logged_in,
+        has_more=has_more,
+    ))
 
 
 # Note that we need the URL to be signed, as this changes the db.
@@ -13,12 +36,22 @@ def get_posts():
 def add_post():
     """Here you get a new post and add it.  Return what you want."""
     # Implement me!
-    return response.json(dict())
+    p_id = db.post.insert(
+        user_email = request.vars.user_email,
+        post_content = request.vars.post_content,
+        created_on = request.vars.created_on,
+        updated_on = request.vars.updated_on,
+    )
+
+    p = db.track(p_id)
+    return response.json(dict(post = p))
 
 
 @auth.requires_signature()
 def del_post():
     """Used to delete a post."""
     # Implement me!
-    return response.json(dict())
+    db(db.post.id == request.vars.post_id).delete()
+    # return response.json(dict())
+    return
 
--- static/js/default_index.js
+++ static/js/default_index.js
@@ -13,6 +13,71 @@ var app = function() {
         }
     };
 
+    //get posts
+    self.get_posts = function(){
+        $.getJSON(get_posts_url(0,20), function(data){
+                self.vue.posts = data.posts;
+                self.vue.has_more = data.has_more;
+                self.vue.logged_in = data.logged_in;
+            })
+    };
+
+    //get posts url
+    function get_posts_url(start_i, end_i){
+        var pp = {
+            start_i: start_i,
+            end_i: end_i,
+        };
+        return posts_url + "?" + $.param(pp);
+    }
+
+    //get more
+    self.get_more = function() {
+        var num_posts = self.vue.posts.length;
+        $.getJSON(get_posts_url(num_posts, num_posts + 20), function(data){
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.posts, data.posts);
+        });
+    };
+
+    //add post button
+    self.add_post_button = function(){
+        self.vue.is_adding_post = !self.vue.is_adding_post;
+    };
+
+    //add post
+    self.add_post = function() {
+        $.post(add_post_url,
+            {
+                // data being sent to server
+                text: self.vue.form_text
+            },
+            function(data){
+                $.web2py.enableElement($("#add_post_submit"));
+                self.vue.posts.unshift(data.post);
+            });
+    };
+
+    //delete post
+    self.delete_post = function(){
+        $.post(del_post_url,
+            {
+                post_id: post_id
+            },
+            function(){
+                var idx = null;
+                for(var i = 0; i < self.vue.tracks.length; i++){
+                    if(self.vue.posts[i].id === post_id){
+                        idx = i + 1;
+                        break;
+                    }
+                }
+                if(idx){
+                    self.vue.posts.splice(idx-1, 1);
+                }
+            }
+        )
+    };
 
     // Complete as needed.
     self.vue = new Vue({
@@ -20,13 +85,22 @@ var app = function() {
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
-            has_more: false
+            is_adding_post: false,
+            has_more: false,
+            logged_in: false,
+            form_text: null
         },
         methods: {
-            get_more: self.get_more
+            get_more: self.get_more,
+            add_post_button: self.add_post_button,
+            add_post: self.add_post,
+            delete_post: self.delete_post,
         }
 
     });
+
+    self.get_posts();
+    $("#vue-div").show();
 
 
     return self;
